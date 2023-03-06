f01be6eec16576b4d2812d64b2c9824901a3ec0d
Miks_Zeltins_221RDB123 <97308677+MiksZelt@users.noreply.github.com>
2023-03-06 10:11:24 +0000

    221RDB123

--- tree_height.py
+++ tree_height.py
@@ -7,35 +7,39 @@ import numpy as np
 
 
 def compute_height(sk, parents):
-    pos = np.zeros([sk], dtype = np.int32)
-    max_h_arr = np.zeros([sk], dtype = np.int32)
+    # 2 arrays
+    pos = np.zeros([sk], dtype = int)
+    max_h_arr = np.zeros([sk], dtype = int)
 
     for i in range(sk):
         if pos[i] == 0:
             node = i
             rep = 0
             while node != -1:
+                if max_height[node] != 0:
+                    rep += max_height[node]
+                    break
                 pos[node] = 1
                 rep = rep + 1
                 max_h_arr[i] = rep
                 node = parents[node]
-
     return max(max_h_arr)
 
 def main():
 
     inp = input()
-
+    # capital I code
     if "I" in inp:
         sk = int(input())
         parents = (list(map(int, input().strip().split())))
         print(compute_height(sk, parents))
 
-
+    # capital F code
     elif "F" in inp:
         file_name = input()
 
         if "a" in file_name:
+            print("Error")
             return
         file = "test/" + file_name
 
